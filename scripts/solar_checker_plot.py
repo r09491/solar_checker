Fixed offset calculation

scripts/solar_checker_plot.py
@@ -76,9 +76,9 @@ def plot_powers(time, smp, ivp1, ivp2, sme, ive1, ive2, price):
     axes[1].minorticks_on()
     title = f'Energy Check #'
     if len(sme) > 0 and sme[-1] >= 0:
-        title += f' Tasmota {sme[-1]:.1f}kWh > {(sme[-1]*price):.2f}€'
+        title += f' Tasmota {sme[-1]:.1f}kWh ~ {(sme[-1]*price):.2f}€'
     if len(ive) > 0 and ive[-1] >= 0:
-        title += f' | APsystems {ive[-1]:.3f}kWh > {ive[-1]*price:.2f}€'
+        title += f' | APsystems {ive[-1]:.3f}kWh ~ {ive[-1]*price:.2f}€'
     axes[1].set_title(title, fontsize='x-large')
     axes[1].set_ylabel('Work [Wh]')
     axes[1].xaxis.set_major_formatter(dformatter)
@@ -114,18 +114,21 @@ def check_powers(price, f = sys.stdin):
 
     """ The normalised smartmeter energy samples """
     sme = np.array(df.SME.apply(str2float))
-    # Get rid of offset
-    sme[sme>=sme[0]] -= sme[0]
-    # Fill up the tail with latest valid value
+    # Get rid of offset and fill tail
+    sme -= sme[0]
+    sme[sme<0.0] = 0.0
     sme[np.argmax(sme)+1:] = sme[np.argmax(sme)]
 
     """ The normalised inverter energy samples channel 1 """
     ive1 = np.array(df.IVE1.apply(str2float))
-    ive1[ive1>=ive1[0]] -= ive1[0]
+    ive1 -= ive1[0]
+    ive1[ive1<0.0] = 0.0
     ive1[np.argmax(ive1)+1:] = ive1[np.argmax(ive1)]
+    
     """ The normalised inverter energy samples channel 2 """
     ive2 = np.array(df.IVE2.apply(str2float))
-    ive2[ive2>=ive2[0]] -= ive2[0]
+    ive2 -= ive2[0]
+    ive2[ive2<0.0] = 0.0
     ive2[np.argmax(ive2)+1:] = ive2[np.argmax(ive2)]
 
     """ ! All arrays are expected to have the same length ! """