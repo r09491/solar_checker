127e0f465a34c3658b4c0b90bfc8faa22e832ae6
r09491 <r09491@gmail.com>
2024-01-20 09:05:19 +0100

    Renamed some aliases

--- scripts/solar_checker_aliases.sh
+++ scripts/solar_checker_aliases.sh
@@ -1,6 +1,6 @@
 # To run in interactive shells SOLAR_CHECKER_STORE_DIR is defined in '.bashrc'
-
-alias tail_apsystems_latest="tail -f \$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_\$(date +%y%m%d).log | \
+# (P = Power, EN = Energy current Day, EL = Energy Lifetime)
+alias apsystems_latest_tail="tail -f \$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_\$(date +%y%m%d).log | \
   awk -F',' 'BEGIN{price=0.369}{printf(\"P %03.0f+%03.0f=%03.0fW  ED %.3f+%.3f=%.3fkWh|%.2f€  EL %.1f+%.1f=%.1fkWh|%.2f€\n\", \$4,\$7,\$4+\$7, \$5,\$8,\$5+\$8,(\$5+\$8)*price, \$6, \$9, \$6+\$9, (\$6+\$9)*price)}'"
 
 alias apsystems_watts="tail -n 5 \$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_\$(date +%y%m%d).log | \
@@ -19,9 +19,9 @@ alias apsystems_watt_hours_lifetime="tail -n 5 \$SOLAR_CHECKER_STORE_DIR/solar_c
 			    	      --title 'Apsystems Inverter Lifetime Watt-Hours (Channel 1,Channel 2)'"
 
 
-#based on the energy at the start of the year
-alias tail_tasmota_latest="tail -f \$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_\$(date +%y%m%d).log| \
-    awk -F',' 'BEGIN{start=4192;price=0.369}{printf(\"P %04.0fW  EJ %.1fkWh|%.2f€ EL %.1fkWh|%.2f€\n\",\$2, \$3-start, (\$3-start)*price, \$3, \$3*price)}'"
+#based on the energy at the start of the year (P = Power, EY = Energy current Year, EL = Energy Lifetime)
+alias tasmota_latest_tail="tail -f \$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_\$(date +%y%m%d).log| \
+    awk -F',' 'BEGIN{start=4192;price=0.369}{printf(\"P %04.0fW  EY %.1fkWh|%.2f€ EL %.1fkWh|%.2f€\n\",\$2, \$3-start, (\$3-start)*price, \$3, \$3*price)}'"
 
 alias tasmota_watts="tail -n 5 \$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_\$(date +%y%m%d).log | \
       			    awk -F',' '{ printf(\"%d,%f\n\", NR, \$2)}' | \
