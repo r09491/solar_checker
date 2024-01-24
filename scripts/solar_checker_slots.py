a7d855cdbff5c0669cab022760f61233e6e2b2de
r09491 <r09491@gmail.com>
2024-01-24 12:46:08 +0100

    mypy passed

--- scripts/solar_checker_slots.py
+++ scripts/solar_checker_slots.py
@@ -26,22 +26,28 @@ import numpy as np
 
 from datetime import datetime, timedelta
 
+from typing import Any
+from dataclasses import dataclass
+
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(os.path.basename(sys.argv[0]))
 
 
-def iso2date(value):
+def iso2date(value: str) -> np.datetime64:
     dt = datetime.fromisoformat(value)
-    return np.datetime64(datetime(year=1900, month=1, day=1, minute=dt.minute, hour=dt.hour))
+    return np.datetime64(datetime(year=1900, month=1, day=1,
+                                  minute=dt.minute, hour=dt.hour))
 
 
-def str2float(value):
+def str2float(value:str) -> float:
     return float(value)
 
 
         
-def show_slots(slots, starts, stops, f = sys.stdin):
+def show_slots(slots: list[str],
+               starts: list[np.datetime64],
+               stops: list[np.datetime64], f: Any = sys.stdin) -> int:
     sep = ','
     names = 'TIME,SMP,SME,IVP1,IVE1,IVTE1,IVP2,IVE2,IVTE2,SPP'.split(',')
     df = pd.read_csv(f, sep = sep, names = names)
@@ -67,35 +73,42 @@ def show_slots(slots, starts, stops, f = sys.stdin):
 
     """ The normalised smartplug power """
     spp = np.array(df.SPP.apply(str2float))
-    if np.isnan(spp).any():
-        spp = None
         
     for slot, start, stop in zip(slots, starts, stops):
         wheres, = np.where((time >= start) & (time < stop))
         if wheres.size == 0: continue
         
-        smps, ivps = smp[wheres],ivp[wheres]
-        spps = spp[wheres] if spp is not None else None
+        smps, ivps, spps = smp[wheres], ivp[wheres], spp[wheres]
 
         text = f"'{slot}'"
         text += f" > smp={smps.mean():.0f}^{smps.max():.0f}W"
         text += f" | ivp={ivps.mean():.0f}^{ivps.max():.0f}W"
-        if spps is not None:
-            text += f" | spp={spps.mean():.0f}^{spps.max():.0f}W"
+        text += f" | spp={spps.mean():.0f}^{spps.max():.0f}W"
         logger.info(text)
 
     return 0
 
 
-def hm2time(hm):
-    return datetime.strptime(hm, "%H:%M")
+def hm2time(hm: str) -> np.datetime64:
+    return np.datetime64(datetime.strptime(hm, "%H:%M"))
 
 
-def parse_args():
+@dataclass
+class Script_Arguments:
+    midnight: np.datetime64
+    morning: np.datetime64
+    noon: np.datetime64
+    afternoon: np.datetime64
+    evening: np.datetime64
+    night: np.datetime64
+    daystop: np.datetime64
+
+def parse_args() -> Script_Arguments:
     description='Show some statistics for time slots'
     parser = argparse.ArgumentParser(description)
 
-    parser.add_argument('--version', action = 'version', version = __version__)
+    parser.add_argument('--version', action = 'version',
+                        version = __version__)
     
     parser.add_argument(
         '--morning', type=hm2time, default = "07:00",
@@ -116,11 +129,16 @@ def parse_args():
     parser.add_argument(
         '--night', type=hm2time, default = "22:00",
         help = "The start hour of the midnight slot")
+
+    args = parser.parse_args()
     
-    return parser.parse_args()
+    return Script_Arguments(hm2time("00:00"),
+                            args.morning, args.noon,
+                            args.afternoon, args.evening,
+                            args.night,hm2time("23:59"))
 
 
-def main():
+def main() -> int:
     args = parse_args()
 
     times = {
@@ -133,23 +151,24 @@ def main():
         "daystop" : hm2time("23:59")
     }
 
+    
     slots = list((times.keys()))
-    starts = np.array([t for _, t in list(times.items())[:-1]])
-    stops = np.array([t for _, t in list(times.items())[1:]])
+    starts = [t for _, t in list(times.items())[:-1]]
+    stops = [t for _, t in list(times.items())[1:]]
 
     for slot, start, stop in zip(slots, starts, stops):
         text = f"'{slot}'"
-        text += f" from '{start.strftime('%H:%M')}'"
-        text += f" to '{stop.strftime('%H:%M')}'"
+        text += f" from '{start.astype(datetime).strftime('%H:%M')}'"
+        text += f" to '{stop.astype(datetime).strftime('%H:%M')}'"
         logger.info(text)
         
-    if (starts > stops).any():
-        si = np.argmax(starts>stops)
-        logger.error(f"Wrong order of slot times '{slots[si]}' is '{starts[si].strftime('%H:%M')}'")
+    if (np.array(starts) > np.array(stops)).any():
+        si = np.argmax(np.array(starts)>np.array(stops))
+        logger.error(f"Wrong order of slot times '{slots[si]}' is '{starts[si].astype(datetime).strftime('%H:%M')}'")
         return 1
     
     try:
-        show_slots(slots, starts.astype(np.datetime64), stops.astype(np.datetime64))
+        show_slots(slots, starts, stops)
     except KeyboardInterrupt:
         pass
     
