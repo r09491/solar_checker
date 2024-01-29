98ecd66ea2fe9b48cb452c9990a0db03671eb05d
r09491 <r09491@gmail.com>
2024-01-29 15:46:56 +0100

    Renamed schedule to switch

--- scripts/solar_checker_switch_once_ecoflow.sh
+++ scripts/solar_checker_switch_once_ecoflow.sh
@@ -2,7 +2,7 @@
 
 SOLAR_CHECKER_ERROR=$SOLAR_CHECKER_STORE_DIR/solar_checker_error_$(date +\%y\%m\%d).log
 SOLAR_CHECKER_LATEST=$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_$(date +\%y\%m\%d).log
-tail -n 15 $SOLAR_CHECKER_LATEST|solar_checker_schedule_once.py \
+tail -n 15 $SOLAR_CHECKER_LATEST|solar_checker_switch_once.py \
      --plug_name plug2 \
      --power_mean_open 350 \
      --power_mean_closed 400 2>> $SOLAR_CHECKER_ERROR
