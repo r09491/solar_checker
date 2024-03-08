ba145f2562202bc952221685c9aad8998d874117
r09491 <r09491@gmail.com>
2024-03-08 19:35:47 +0100

    Changed some scripts

--- scripts/solar_checker_switch_on_export_once_plug1.sh
+++ scripts/solar_checker_switch_on_export_once_plug1.sh
@@ -6,8 +6,8 @@ tail -n 11 $SOLAR_CHECKER_LATEST|solar_checker_switch_on_export_once.py \
      --plug_name plug1 \
      --power_mean_import_open 20 \
      --power_mean_export_closed 20 \
-     --power_mean_deviation 11 \
-     --power_samples 13 2>> $SOLAR_CHECKER_ERROR
+     --power_mean_deviation 15 \
+     --power_samples 11 2>> $SOLAR_CHECKER_ERROR
 
 # This script shall start/stop the charging of low power devices. It
 # starts the charging if power is exported after a certain period (13
