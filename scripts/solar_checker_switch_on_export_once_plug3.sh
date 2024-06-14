052c4f4a19cabcb952952fc138b16aa734b559bf
r09491 <r09491@gmail.com>
2024-06-14 08:37:08 +0200

    Updated export scripts

--- scripts/solar_checker_switch_on_export_once_plug3.sh
+++ scripts/solar_checker_switch_on_export_once_plug3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
-SAMPLES=5
+SAMPLES=2
 SOLAR_CHECKER_ERROR=$SOLAR_CHECKER_STORE_DIR/solar_checker_error_$(date +\%y\%m\%d).log
 SOLAR_CHECKER_LATEST=$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_$(date +\%y\%m\%d).log
 tail -n $SAMPLES $SOLAR_CHECKER_LATEST|solar_checker_switch_on_export_once.py \
      --plug_name plug3 \
-     --power_mean_import_open 10 \
-     --power_mean_export_closed 200 \
+     --power_mean_import_open 50 \
+     --power_mean_export_closed 150 \
      --power_samples $SAMPLES 2>> $SOLAR_CHECKER_ERROR
 
 # This script shall start/stop the charging of my ecoflow. Asumed charge rate 200!
