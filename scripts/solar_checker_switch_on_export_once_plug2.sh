a78cd34f7d433eb3200de58d965c871da2f7495f
r09491 <r09491@gmail.com>
2024-06-25 09:53:26 +0200

    Adapted plug thresholds

--- scripts/solar_checker_switch_on_export_once_plug2.sh
+++ scripts/solar_checker_switch_on_export_once_plug2.sh
@@ -6,7 +6,7 @@ SOLAR_CHECKER_LATEST=$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_$(date +\%y\%
 tail -n $SAMPLES $SOLAR_CHECKER_LATEST|solar_checker_switch_on_export_once.py \
      --plug_name plug2 \
      --power_mean_import_open 10 \
-     --power_mean_export_closed 20 \
+     --power_mean_export_closed 25 \
      --power_samples $SAMPLES 2>> $SOLAR_CHECKER_ERROR
 
 # This script shall start/stop the charging of my laptop.
--- scripts/solar_checker_switch_on_export_once_plug3.sh
+++ scripts/solar_checker_switch_on_export_once_plug3.sh
@@ -5,7 +5,7 @@ SOLAR_CHECKER_ERROR=$SOLAR_CHECKER_STORE_DIR/solar_checker_error_$(date +\%y\%m\
 SOLAR_CHECKER_LATEST=$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_$(date +\%y\%m\%d).log
 tail -n $SAMPLES $SOLAR_CHECKER_LATEST|solar_checker_switch_on_export_once.py \
      --plug_name plug3 \
-     --power_mean_import_open 50 \
+     --power_mean_import_open 20 \
      --power_mean_export_closed 150 \
      --power_samples $SAMPLES 2>> $SOLAR_CHECKER_ERROR
 
