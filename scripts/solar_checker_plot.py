746c7b912ccdebd5cb64e86514f13b41de9ff9d6
r09491 <r09491@gmail.com>
2024-07-04 20:33:38 +0200

    Fixed plot script

--- scripts/solar_checker_plot.py
+++ scripts/solar_checker_plot.py
@@ -26,6 +26,7 @@ import asyncio
 
 import io
 import base64
+import numpy as np
 
 import matplotlib
 matplotlib.use('TkAgg', force=True)
@@ -40,58 +41,93 @@ from dataclasses import dataclass
 from typing import Any
 from utils.types import f64, f64s, t64, t64s, strings, timeslots
 from utils.samples import get_columns_from_csv
-from utils.plots import get_w_line, get_kwh_line, XSIZE, YSIZE
+from utils.plots import get_blocks, get_w_line, get_kwh_line, XSIZE, YSIZE
 
 
 import logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s',
     datefmt='%H:%M:%S',)
 logger = logging.getLogger(os.path.basename(sys.argv[0]))
 
 
-async def get_w (
+async def get_blocks_png(time: t64, smp: f64,
+                     ivp1: f64, ivp2: f64, spph: f64,
+                     sbpi: f64s, sbpo: f64s, sbpb: f64,
+                     spp1: f64, spp2: f64, spp3: f64, spp4: f64):
+    logger.info('Decoding blocks image')
+    bytes = io.BytesIO(base64.b64decode(await get_blocks(**vars())))
+    blocks_png = mpimg.imread(bytes, format='png')
+    logger.info('Decoded blocks image')
+    return blocks_png
+
+
+async def get_w_png (
         time: t64s, smp: f64s,
-        ivp1: f64s, ivp2: f64s, spp: f64s) -> Any:
+        ivp1: f64s, ivp2: f64s, spph: f64s,
+        sbpi: f64s, sbpo: f64s, sbpb: f64s) -> Any:
     logger.info('Decoding power image')
-    w = await get_w_line(time, smp, ivp1, ivp2, spp)
-    w = base64.b64decode(w)
-    w = io.BytesIO(w)
-    w = mpimg.imread(w, format='png')
+    bytes = io.BytesIO(base64.b64decode(await get_w_line(**vars())))
+    w_png = mpimg.imread(bytes, format='png')
     logger.info('Decoded power image')
-    return w
+    return w_png
 
 
-async def get_wh (
-        time: t64s, sme: f64s,
-        ive1: f64s, ive2: f64s,
-        spp: f64s, price: f64) -> Any:
+async def get_kwh_png (
+        time: t64s, smeon: f64s, smeoff: f64s,
+        ive1: f64s, ive2: f64s, speh: f64s,
+        sbei: f64s, sbeo: f64s, sbeb: f64s, sbsb: f64s,
+        empty_kwh: f64, full_kwh: f64, price: f64) -> Any:
     logger.info('Decoding energy image')
-    wh = await get_kwh_line(time, sme, ive1, ive2, spp, price)
-    wh = base64.b64decode(wh)
-    wh = io.BytesIO(wh)
-    wh = mpimg.imread(wh, format='png')
+    bytes = io.BytesIO(base64.b64decode(await get_kwh_line(**vars())))
+    wh_png = mpimg.imread(bytes, format='png')
     logger.info('Decoded energy image')
-    return wh
+    return wh_png
 
-async def get_images(c: dict, price: f64) -> Any:
+
+async def get_images(c: dict, empty_kwh:f64, full_kwh:f64, price: f64) -> Any:
     logger.info(f'get_images started')
 
-    time, spp = c['TIME'], c['SPP']
-    sme, ive1, ive2 = c['SME'], c['IVE1'], c['IVE2']
+    time, spph = c['TIME'], c['SPPH']
+    ##sme, ive1, ive2 = c['SME'], c['IVE1'], c['IVE2']
     smp, ivp1, ivp2 = c['SMP'], c['IVP1'], c['IVP2']
+    sbpi, sbpo, sbpb, sbsb = c['SBPI'], c['SBPO'], c['SBPB'], c['SBSB']
+    spp1, spp2, spp3, spp4 = c['SPP1'], c['SPP2'], c['SPP3'], c['SPP4']
 
-    w, wh = await asyncio.gather(
-        get_w(time, smp, ivp1, ivp2, spp),
-        get_wh(time, sme, ive1, ive2, spp, price),
-    )
-
+    smpon = np.zeros_like(smp)
+    smpon[smp>0] = smp[smp>0]
+    smpoff = np.zeros_like(smp)
+    smpoff[smp<=0] = -smp[smp<=0]
+    
+    blocks_png, w_png, kwh_png = await asyncio.gather(
+        get_blocks_png(time[-1], smp[-1], ivp1[-1], ivp2[-1],
+                       spph[-1] if spph is not None else 0,
+                       sbpi[-1] if sbpi is not None else 0,
+                       sbpo[-1] if sbpo is not None else 0,
+                       sbpb[-1] if sbpb is not None else 0,
+                       spp1[-1] if spp1 is not None else 0,
+                       spp2[-1] if spp2 is not None else 0,
+                       spp3[-1] if spp3 is not None else 0,
+                       spp4[-1] if spp4 is not None else 0),
+        get_w_png(time, smp, ivp1, ivp2,
+                   spph, sbpi, sbpo, sbpb),
+        get_kwh_png(time,
+            smpon.cumsum()/1000/60 if smpon is not None else None,
+            smpoff.cumsum()/1000/60 if smpoff is not None else None,
+            ivp1.cumsum()/1000/60 if ivp1 is not None else None,
+            ivp2.cumsum()/1000/60 if ivp2 is not None else None,
+            spph.cumsum()/1000/60 if spph is not None else None,
+            sbpi.cumsum()/1000/60 if sbpi is not None else None,
+            sbpo.cumsum()/1000/60 if sbpo is not None else None,
+            sbpb.cumsum()/1000/60 if sbpb is not None else None,
+            sbsb*full_kwh if sbsb is not None else None,
+            empty_kwh, full_kwh, price))
     logger.info(f'get_images done')
-    return w, wh
+    return blocks_png, w_png, kwh_png
 
 
-async def check_powers(price: f64) -> int:
+async def check_powers(empty_kwh:f64, full_kwh:f64, price: f64) -> int:
 
     # Read from stdin
     c = await get_columns_from_csv()
@@ -100,25 +136,37 @@ async def check_powers(price: f64) -> int:
         return 1
 
     plt.switch_backend('Agg')    
-    w, wh = await get_images(c, price)
+    blocks, w, kwh = await get_images(c, empty_kwh, full_kwh,price)
     plt.switch_backend('TkAgg')
 
     logger.info('Plotting started')
-    
-    fig, axes = plt.subplots(
-        nrows = 2, sharex = True, figsize = (2*XSIZE,4*YSIZE))
 
-    text = f'Solar Checker'
-    fig.text(0.5, 0.0, text, ha='center', fontsize='x-large')
+    fig, overview = plt.subplots(
+        nrows = 1, sharex = True, figsize = (2*XSIZE,2*YSIZE))
+    fig.tight_layout(pad=2.0)
+    
+    overview.clear()
+    overview.imshow(blocks)
+    overview.set_axis_off()
+    
+    fig, power = plt.subplots(
+        nrows = 1, sharex = True, figsize = (2*XSIZE,2*YSIZE))
     fig.tight_layout(pad=2.0)
 
-    axes[0].clear()
-    axes[0].imshow(w)
-    axes[0].set_axis_off()
+    ##text = f'Solar Checker'
+    ##fig.text(0.5, 0.0, text, ha='center', fontsize='x-large')
 
-    axes[1].clear()
-    axes[1].imshow(wh)
-    axes[1].set_axis_off()
+    power.clear()
+    power.imshow(w)
+    power.set_axis_off()
+
+    fig, energy = plt.subplots(
+        nrows = 1, sharex = True, figsize = (2*XSIZE,2*YSIZE))
+    fig.tight_layout(pad=2.0)
+    
+    energy.clear()
+    energy.imshow(kwh)
+    energy.set_axis_off()
 
     logger.info('Plotting done')
 
@@ -129,6 +177,8 @@ async def check_powers(price: f64) -> int:
 
 @dataclass
 class Script_Arguments:
+    empty_kwh: f64
+    full_kwh: f64
     price: f64
 
 def parse_arguments() -> Script_Arguments:
@@ -141,11 +191,17 @@ def parse_arguments() -> Script_Arguments:
     parser.add_argument('--version',
                         action = 'version', version = __version__)
 
+    parser.add_argument('--empty_kwh', type = f64, default = 0.160,
+                        help = "The battery empty kWh")
+
+    parser.add_argument('--full_kwh', type = f64, default = 1.600,
+                        help = "The battery full kWh")
+
     parser.add_argument('--price', type = f64, default = 0.369,
                         help = "The price of energy per kWh")
 
     args = parser.parse_args()
-    return Script_Arguments(args.price)
+    return Script_Arguments(args.empty_kwh,args.full_kwh,args.price)
 
 
 def main() -> int:
@@ -154,7 +210,7 @@ def main() -> int:
     err = 0
     
     try:
-        err = asyncio.run(check_powers(args.price))
+        err = asyncio.run(check_powers(args.empty_kwh, args.full_kwh, args.price))
     except KeyboardInterrupt:
         pass
     except TypeError:
