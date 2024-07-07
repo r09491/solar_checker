a58870782a8d307e2ec4431a7e9dc803c4e47cbe
r09491 <r09491@gmail.com>
2024-07-07 14:26:06 +0200

    Fixed slot scripts

--- scripts/solar_checker_slots.py
+++ scripts/solar_checker_slots.py
@@ -102,15 +102,15 @@ def parse_args() -> Script_Arguments:
         help = "The start hour of the noon slot")
 
     parser.add_argument(
-        '--afternoon', type=hm2time, default = "14:00",
+        '--afternoon', type=hm2time, default = "12:00",
         help = "The start hour of the afternoon slot")
 
     parser.add_argument(
-        '--evening', type=hm2time, default = "17:00",
+        '--evening', type=hm2time, default = "18:00",
         help = "The start hour of the evening slot")
 
     parser.add_argument(
-        '--night', type=hm2time, default = "22:00",
+        '--night', type=hm2time, default = "23:00",
         help = "The start hour of the midnight slot")
 
     args = parser.parse_args()
--- scripts/solar_checker_slots_yesterday.sh
+++ scripts/solar_checker_slots_yesterday.sh
@@ -4,6 +4,6 @@
 #APSYSTEMS=$SOLAR_CHECKER_STORE_DIR/apsystems_latest_$(date -d "yesterday" +\%y\%m\%d).log
 #paste -d',' $TASMOTA $APSYSTEMS|solar_checker_slots.py
 SOLAR_CHECKER=$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_$(date -d "yesterday" +\%y\%m\%d).log
-cat $SOLAR_CHECKER|solar_checker_slots.py --price 0.369
+cat $SOLAR_CHECKER|solar_checker_slots.py
 
 
