2e35f0cfcf6e34a45dd92edf468ba18da344cbb4
r09491 <r09491@gmail.com>
2024-08-18 12:10:04 +0200

    Changed some thresholds

--- scripts/solar_checker_home_load_set_once.sh
+++ scripts/solar_checker_home_load_set_once.sh
@@ -15,7 +15,7 @@
 ## 4-59/5 * * * * sleep 30 && solar_checker_switch_on_export_once_plug1.sh
 ##
 
-SAMPLES=2
+SAMPLES=3
 SOLAR_CHECKER_ERROR=$SOLAR_CHECKER_STORE_DIR/solar_checker_error_$(date +\%y\%m\%d).log
 SOLAR_CHECKER_LATEST=$SOLAR_CHECKER_STORE_DIR/solar_checker_latest_$(date +\%y\%m\%d).log
 tail -n $SAMPLES $SOLAR_CHECKER_LATEST|solar_checker_home_load_set_once.py \
