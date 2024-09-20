6a8e46560a4471c833de9ec051ba1c58fefc9ff6
r09491 <r09491@gmail.com>
2024-09-20 10:27:15 +0200

    Graph if solix is off

--- utils/plots.py
+++ utils/plots.py
@@ -657,27 +657,35 @@ def _get_blocks(time: t64, smp: f64,
                         show_power = sbpi+sbpb>ivp1+ivp2)
 
     
-    if (ivp1>0) or (ivp1<1 and ivp2<1 and sbpo >0):
+    if (sbpo>0) and (ivp1>0):
         _add_link_to_ax(ax, *solix_out, 'E', *inv_mppt_1, 'W',
                         ivp1 if ivp1>0 else sbpo/2,
                         'c' if ivp1>0 else 'grey',
                         show_power = ivp1>0)
+    elif (ivp1>0):
+        _add_link_to_ax(ax, *panel_1, 'N', *inv_mppt_1, 'N',
+                        ivp1, 'c',
+                        show_power = ivp1>0)
+        
+    _add_link_to_ax(ax, *inv_mppt_1, 'S', *plugh, 'N',
+                    ivp1 if ivp1>0 else sbpo/2,
+                    'c' if ivp1>0 else 'grey',
+                    show_power = False)            
 
-        _add_link_to_ax(ax, *inv_mppt_1, 'S', *plugh, 'N',
-                        ivp1 if ivp1>0 else sbpo/2,
-                        'c' if ivp1>0 else 'grey',
-                        show_power = False)            
-
-    if (ivp2>0) or (ivp1<1 and ivp2<1 and sbpo >0):
+    if  (sbpo >0) and  (ivp2>0):
         _add_link_to_ax(ax, *solix_out, 'E', *inv_mppt_2, 'W',
                         ivp2 if ivp2>0 else sbpo/2,
                         'c' if ivp2>0 else 'grey',
                         show_power = ivp2>0)
+    elif (ivp2>0):
+        _add_link_to_ax(ax, *panel_2, 'S', *inv_mppt_2, 'S',
+                        ivp2, 'c',
+                        show_power = ivp2>0)
 
-        _add_link_to_ax(ax, *inv_mppt_2, 'N', *plugh, 'S',
-                        ivp2 if ivp2>0 else sbpo/2,
-                        'c' if ivp2>0 else 'grey',
-                        show_power = False)
+    _add_link_to_ax(ax, *inv_mppt_2, 'N', *plugh, 'S',
+                    ivp2 if ivp2>0 else sbpo/2,
+                    'c' if ivp2>0 else 'grey',
+                    show_power = False)
 
     
     ivp = (ivp1+ivp2)
