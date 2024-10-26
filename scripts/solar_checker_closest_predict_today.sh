595bf3b80190ece9e9ce66649122eb449f13f8d5
r09491 <r09491@gmail.com>
2024-10-26 20:16:08 +0200

    Allow logs over last two months over years

--- scripts/solar_checker_closest_predict_today.sh
+++ scripts/solar_checker_closest_predict_today.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -l
 
+M0=$(date -d "0 month ago" +\%m)
+M1=$(date -d "1 month ago" +\%m)
+SOLAR_CHECKER_LOGFORMAT="??[${M0:0:1}${M1:0:1}][${M0:1:1}${M1:1:1}]??"
 SOLAR_CHECKER_PREFIX="solar_checker_latest"
-SOLAR_CHECKER_LOGFORMAT="24*"
 SOLAR_CHECKER_LOGDAY=$(date -d "0 day ago" +\%y\%m\%d)
 #SOLAR_CHECKER_STARTTIME=$(date -d "3 hour ago" +\%H:\%M) 
 SOLAR_CHECKER_STOPTIME=$(date +\%H:\%M) 
