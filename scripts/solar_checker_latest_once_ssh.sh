1ca571e602453d32d778ce9c17ffc79c60ff416c
r09491 <r09491@gmail.com>
2025-02-06 14:18:31 +0100

    Fixed ssh script

--- scripts/solar_checker_latest_once_ssh.sh
+++ scripts/solar_checker_latest_once_ssh.sh
@@ -1,8 +1,8 @@
-solar_checker_latest_once.py --iv_ip "192.168.101.48" \
-			     --sm_ip "192.168.101.30" \
+solar_checker_latest_once.py --iv_ip "apsystems" \
+			     --sm_ip "tasmota" \
 			     --sp_switch_4 "plug0" \
 			     --sp_switch_3 "plug3" \
 			     --sp_switch_2 "plug2" \
 			     --sp_switch_1 "plug1" \
-			   >1 >(ssh r09491@wanderer "cat >> solar_checker_latest_$(date +\%y\%m\%d).log") \
-			   >2 >(ssh r09491@wanderer "cat >> solar_checker_error_$(date +\%y\%m\%d).log")
+			     1> >(ssh r09491@pipi "cat >> 0WORK/solar_checker_latest_$(date +\%y\%m\%d).log") \
+			     2> >(ssh r09491@pipi "cat >> 0WORK/solar_checker_error_$(date +\%y\%m\%d).log")
