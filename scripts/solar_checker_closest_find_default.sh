b88482cddee854f093428d2ff2a83f741d0d7764
r09491 <r09491@gmail.com>
2025-08-11 21:35:38 +0200

    Removed debug output

--- scripts/solar_checker_closest_find_default.sh
+++ scripts/solar_checker_closest_find_default.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -l
 
-echo $PATH
-
 M0=$(date -d "0 month" +\%m)
 M1=$(date -d "1 month" +\%m)
 SOLAR_CHECKER_LOGFORMAT="??[${M0:0:1}${M1:0:1}][${M0:1:1}${M1:1:1}]??"
@@ -9,7 +7,7 @@ SOLAR_CHECKER_PREFIX="solar_checker_latest"
 SOLAR_CHECKER_LOGDAY=$(date -d "0 day ago" +\%y\%m\%d)
 SOLAR_CHECKER_STARTTIME=$(date -d "3 hour ago" +\%H:\%M) 
 SOLAR_CHECKER_STOPTIME=$(date -d "0 hour ago" +\%H:\%M) 
-$HOME/.venv/bin/solar_checker_closest_predict.py \
+solar_checker_closest_predict.py \
     --logday $SOLAR_CHECKER_LOGDAY \
     --logdayformat $SOLAR_CHECKER_LOGFORMAT \
     --logprefix $SOLAR_CHECKER_PREFIX \
