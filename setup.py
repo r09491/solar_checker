53d1d0439a28e59f46610405f4eaf92224a4c1f3
r09491 <sepp.heid@gmail.com>
2025-12-19 11:42:07 +0100

    New relaese

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as f:
     long_description = f.read()
     
 setup(name='solar_checker',
-      version='1.9.0',
+      version='1.9.1',
       description='Solar checker with Ecoflow Delta Max,APsystems,Anker,Tuya and BrightSky API',
       url='https://github.com/r09491/solar_checker',
       author='r09491',
